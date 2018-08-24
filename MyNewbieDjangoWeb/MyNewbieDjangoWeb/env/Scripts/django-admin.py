f85a8e5d641da3701336f5c26d67157104f949ab
NutthapongWongchiangpeng <nutthapong_00@outlook.com>
2018-08-24 17:34:00 +0700

    django

---
+++ MyNewbieDjangoWeb/MyNewbieDjangoWeb/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!D:\myNewbieWeb\MyNewbieDjangoWeb\MyNewbieDjangoWeb\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
